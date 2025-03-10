1e70714800aadfecfbb8db64abaad11837112c00
Varun Saxena <varunsaxena5elc@gmail.com>
2025-03-10 12:22:13 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages , setup
+from typing import List
+
+hyphen = "-e ."
+def get_requirements(file_path:str) -> List[str] :
+    requ=[]
+    with open(file_path) as file_obj :
+        requ = file_obj.readlines()
+        requ = [req.replace("\n","") for req in requ]
+        
+        if hyphen in requ :
+            requ.remove(hyphen)
+
+    return requ
+
+setup(
+name="ml_project",
+version='0.0.1',
+author="Varun",
+author_email="varunsaxena5elc@gmail.com",
+packages=find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
+
